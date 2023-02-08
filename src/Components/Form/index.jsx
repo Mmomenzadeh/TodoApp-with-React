4d6250961ded5b9499c01409c2b8af88e4b195a4
Mmomenzadeh <m.momenzadeh1999@gmail.com>
2023-02-08 23:11:40 +0330

    finish create Todo in reducer

--- src/Components/Form/index.jsx
+++ src/Components/Form/index.jsx
@@ -1,20 +1,48 @@
-import "./Form.css"
+import { useContext } from "react";
+import { TodoContext } from "../../Context";
+import "./Form.css";
 const Form = () => {
-    return ( 
-        <div className="form-ui">
-             <div className="input-container">
-            <div className="input-form">
-                <label>Title</label>
-                <input type="text" placeholder="Title" />
-            </div>
-            <div className="input-form">
-                <label>Describe</label>
-                <input type="text" placeholder="Describe" />
-            </div>
-            </div>
-            <button className="form-btn-ui">Submit</button>
+  const { todoObject, settodoObject, dispatch } = useContext(TodoContext);
+
+  const createTodo = (e) => {
+    e.preventDefault();
+    dispatch({
+      type: "create_todo",
+      payload: {
+        title: todoObject.title,
+        desc: todoObject.desc,
+      },
+    });
+  };
+  return (
+    <div className="form-ui">
+      <div className="input-container">
+        <div className="input-form">
+          <label>Title</label>
+          <input
+            type="text"
+            placeholder="Title"
+            onChange={(e) =>
+              settodoObject({ ...todoObject, title: e.target.value })
+            }
+          />
         </div>
-     );
-}
- 
+        <div className="input-form">
+          <label>Describe</label>
+          <input
+            type="text"
+            placeholder="Describe"
+            onChange={(e) =>
+              settodoObject({ ...todoObject, desc: e.target.value })
+            }
+          />
+        </div>
+      </div>
+      <button className="form-btn-ui" onClick={createTodo}>
+        Submit
+      </button>
+    </div>
+  );
+};
+
 export default Form;
