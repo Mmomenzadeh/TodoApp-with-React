ac155d2634bbbe45ec58d514a2bffeb96f93861f
Mmomenzadeh <m.momenzadeh1999@gmail.com>
2023-02-08 22:32:05 +0330

    finish Todo , TodoList Ui

---
+++ src/Components/TodoList/index.jsx
@@ -0,0 +1,11 @@
+import "./TodoList.css";
+const TodoList = () => {
+  return (
+    <ul className="todoList-ui">
+
+      {/* <div className="line"></div> */}
+    </ul>
+  );
+};
+
+export default TodoList;
