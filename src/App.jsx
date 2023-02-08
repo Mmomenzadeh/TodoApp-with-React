9a24e68c1edf6945a34719cad59da918bb23236b
Mmomenzadeh <m.momenzadeh1999@gmail.com>
2023-02-08 23:11:40 +0330

    finish create Todo in reducer

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import "./App.css"
 import Form from "./Components/Form";
 import TodoList from "./Components/TodoList";
 const App = () => {
+  
+
   return (
      <div className="app-ui">
       <h1>My Todo App</h1>
