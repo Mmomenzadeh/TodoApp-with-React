2a41d5ba17b62bc862c130980a0f9ebd492a8eda
Mmomenzadeh <m.momenzadeh1999@gmail.com>
2023-02-09 22:05:24 +0330

    finish checked btn in reducer and conditional style for title and desc

--- src/Context/index.jsx
+++ src/Context/index.jsx
@@ -1,25 +1,43 @@
 import React, { createContext, useReducer, useState } from "react";
 import { Reducer } from "../Reducer/Reducer";
 
- export const TodoContext = createContext({
-    todoObject:"" , settodoObject:()=>{},
-    todoState :[] , dispatch:(command)=>[],
-    todoInEditeMode:false , settodoInEditeMode:()=>{}
+export const TodoContext = createContext({
+  todoObject: "",
+  settodoObject: () => {},
+  todoState: [],
+  dispatch: (command) => [],
+  todoInEditeMode: false,
+  settodoInEditeMode: () => {},
+  isChecked: false,
+  setIsChecked: () => {},
+});
 
- })
+const TodoContextProvider = ({ children }) => {
+  const [todoState, dispatch] = useReducer(Reducer, []);
+  const initialValue = {
+    title: "",
+    desc: "",
+    id: "",
+    checked: false,
+  };
+  const [todoObject, settodoObject] = useState(initialValue);
+  const [todoInEditeMode, settodoInEditeMode] = useState(false);
+  const [isChecked, setIsChecked] = useState(false);
+  return (
+    <TodoContext.Provider
+      value={{
+        todoObject,
+        settodoObject,
+        todoState,
+        dispatch,
+        todoInEditeMode,
+        settodoInEditeMode,
+        isChecked, setIsChecked
+      }}
+    >
+      {children}
+    </TodoContext.Provider>
+  );
+};
 
-
- const TodoContextProvider=({children})=>{
-   const [todoState , dispatch] = useReducer(Reducer , [])
-    const initialValue={
-        title : "",
-        desc : "",
-        id : "",
-        checked : false
-    }
-    const [todoObject , settodoObject ]=useState(initialValue)
-    const [todoInEditeMode , settodoInEditeMode ]=useState(false)
-    return <TodoContext.Provider value={{ todoObject , settodoObject ,todoState , dispatch ,todoInEditeMode , settodoInEditeMode}}>{children}</TodoContext.Provider>
- }
-
- export default TodoContextProvider
+export default TodoContextProvider;
--- src/Reducer/Reducer.js
+++ src/Reducer/Reducer.js
@@ -14,17 +14,23 @@ export const Reducer = (todoState, action) => {
     case "delete_todo":
       return todoState.filter((todoItem) => todoItem.id !== action.payload.id);
 
-      case "edite_todo":
-        const todoIndex = todoState.findIndex((todoItem)=>todoItem.id === action.payload.id)
-        const UpdateTodo = [...todoState]
-            UpdateTodo.splice(todoIndex , 1 ,{
-                title : action.payload.title,
-                desc : action.payload.desc,
-                id : action.payload.id,
-                checked : false
-            })
-        return  UpdateTodo
+    case "edite_todo":
+      const todoIndex = todoState.findIndex(
+        (todoItem) => todoItem.id === action.payload.id
+      );
+      const UpdateTodo = [...todoState];
+      UpdateTodo.splice(todoIndex, 1, {
+        title: action.payload.title,
+        desc: action.payload.desc,
+        id: action.payload.id,
+        checked: false,
+      });
+      return UpdateTodo;
+
+    case "check_todo":
+      return todoState.map((todoItem) =>(todoItem.id === action.payload.id? { ...todoItem, checked: action.payload.status }: todoItem));
+
     default:
-        return todoState
+      return todoState;
   }
 };
