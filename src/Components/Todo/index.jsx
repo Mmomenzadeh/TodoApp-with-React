7ab30280606013160111e09c940266e375653499
Mmomenzadeh <m.momenzadeh1999@gmail.com>
2023-02-09 22:05:24 +0330

    finish checked btn in reducer and conditional style for title and desc

--- src/Components/Todo/index.jsx
+++ src/Components/Todo/index.jsx
@@ -2,38 +2,71 @@ import { useContext } from "react";
 import { TodoContext } from "../../Context";
 import "./Todo.css";
 
-const Todo = ({todoItem}) => {
+const Todo = ({ todoItem }) => {
+  const {
+    todoObject,
+    settodoObject,
+    dispatch,
+    settodoInEditeMode,
+    todoInEditeMode,
+    isChecked,
+    setIsChecked,
+  } = useContext(TodoContext);
 
-  const {todoObject, settodoObject, dispatch ,settodoInEditeMode ,todoInEditeMode} = useContext(TodoContext)
-
-  const handleTodoDelete =(id)=>{
+  const handleTodoDelete = (id) => {
     dispatch({
-      type : "delete_todo",
-      payload : {
-        id
+      type: "delete_todo",
+      payload: {
+        id,
+      },
+    });
+  };
+  const handleTodoEdite = (id) => {
+    settodoObject(todoItem);
+    settodoInEditeMode(id);
+  };
+  const handleTodoCheck = (id ,status) => {
+    dispatch({
+      type : "check_todo",
+      payload:{
+        id,status
       }
     })
-  }
-  const handleTodoEdite =(id)=>{
-    settodoObject(todoItem)
-    settodoInEditeMode(id)
-  
 
-  }
-  const handleTodoCheck =(id)=>{
-
-  }
+  };
 
   return (
     <li className="todo-ui">
       <div className="todo-content">
-        <h2>{todoItem.title}</h2>
-        <p>{todoItem.desc}</p>
+        <h2 style={{textDecoration : todoItem.checked ? "line-through" : null}}>{todoItem.title}</h2>
+        <p style={{color: todoItem.checked ? "#737474" : "#fff" }}>{todoItem.desc}</p>
       </div>
       <div className="btn-container">
-        <button className="btn-ui" onClick={()=>handleTodoDelete(todoItem.id)}>Delete</button>
-        <button className="btn-ui" onClick={()=>handleTodoEdite(todoItem.id)}>Edite</button>
-        <button className="btn-ui" onClick={()=>handleTodoCheck(todoItem.id)}>Checked</button>
+        <button
+          className="btn-ui"
+          onClick={() => handleTodoDelete(todoItem.id)}
+        >
+          Delete
+        </button>
+        <button className="btn-ui" onClick={() => handleTodoEdite(todoItem.id)}>
+          Edite
+        </button>
+        {todoItem.checked ? (
+          <button
+            className="btn-ui"
+            style={{ width: "100px"}}
+            onClick={() => handleTodoCheck(todoItem.id, false)}
+          >
+            UnChecked
+          </button>
+        ) : (
+          <button
+            className="btn-ui"
+            onClick={() => handleTodoCheck(todoItem.id, true)}
+          >
+            Checked
+          </button>
+        )}
       </div>
     </li>
   );
